493bdcca5fe8d19d850e5873ed1eccb91f0b1145
dimanu-py <diegomtz126@gmail.com>
2024-05-26 14:40:49 +0200

    feat(unicode-file-converter): introduce the concept of a FileReader
    The UnicodeFileToHtmlTextConverter class is violating Single Responsibility principle, having more than one reason to change:  reading the file and processing the conversion

--- html_converter_kata/unicode_file_to_html_text_converter.py
+++ html_converter_kata/unicode_file_to_html_text_converter.py
@@ -1,10 +1,16 @@
 import html as html_converter
 
 
+class FileReader:
+
+    def __init__(self, full_filename_with_path: str) -> None:
+        self.full_filename_with_path = full_filename_with_path
+
+
 class UnicodeFileToHtmlTextConverter:
 
-    def __init__(self, full_filename_with_path):
-        self.full_filename_with_path = full_filename_with_path
+    def __init__(self, full_filename_with_path: str) -> None:
+        self.file_reader = FileReader(full_filename_with_path)
 
     def convert_to_html(self):
         f = open(self.full_filename_with_path, "r")
