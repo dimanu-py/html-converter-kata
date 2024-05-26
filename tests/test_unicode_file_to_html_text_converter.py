714f5ff0f9af43fc47e43f58ea3f4b4fd81086f4
dimanu-py <diegomtz126@gmail.com>
2024-05-26 18:00:50 +0200

    test(unicode-file-converter): verify new line character is converted to br tag

--- tests/test_unicode_file_to_html_text_converter.py
+++ tests/test_unicode_file_to_html_text_converter.py
@@ -20,3 +20,13 @@ class TestUnicodeFileToHtmlTextConverter:
             converted_text,
             options=Options().with_reporter(self.reporter)
         )
+
+    def test_new_line_is_converted_to_br_tag(self, mocker):
+        file_path = "dummy_path.txt"
+        mocker.patch("html_converter_kata.unicode_file_to_html_text_converter.FileReader.read", return_value=["line1", "line2"])
+        converter = UnicodeFileToHtmlTextConverter(file_path)
+        expected_html = "line1<br />line2<br />"
+
+        converted_text = converter.convert_to_html()
+
+        assert converted_text == expected_html
