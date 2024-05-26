45e03dbbe633b0fa7c61d589f581f666bf2eafb4
dimanu-py <diegomtz126@gmail.com>
2024-05-26 14:36:11 +0200

    test(unicode-file-converter): add approval test for UnicodeFileToHtmlTextConverter class
    As I'm not sure how the code should behave, I'll start with approval tests so I can refactor the code in a safe environment until I can implement specific tests with Stubs

--- tests/test_unicode_file_to_html_text_converter.py
+++ tests/test_unicode_file_to_html_text_converter.py
@@ -1,8 +1,22 @@
-import html_converter_kata.unicode_file_to_html_text_converter
+import pytest
+from approvaltests import verify, Options
+
+from html_converter_kata.unicode_file_to_html_text_converter import UnicodeFileToHtmlTextConverter
 
 
-def test_html_converter():
-    # TODO: Write a better test
-    # converter = unicode_file_to_html_text_converter.UnicodeFileToHtmlTextConverter("foo")
-    # assert "foo" == converter.full_filename_with_path
-    pass
+class TestUnicodeFileToHtmlTextConverter:
+
+    @pytest.fixture(autouse=True)
+    def setup_reporter(self, intellij_diff_reporter):
+        self.reporter = intellij_diff_reporter
+
+    def test_html_converter(self):
+        file_to_convert = "test_file.txt"
+        converter = UnicodeFileToHtmlTextConverter(file_to_convert)
+
+        converted_text = converter.convert_to_html()
+
+        verify(
+            converted_text,
+            options=Options().with_reporter(self.reporter)
+        )
